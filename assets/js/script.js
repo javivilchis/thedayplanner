081f1916cd69a7a24e8048d6cbfbcd1206b6ebc1
javier vilchis <javivilchis@gmail.com>
2021-06-08 20:09:15 -0500

    cleaning up my code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,6 @@ $(document).ready( function(){
 var dayPlanner = document.getElementById("dayPlanner");
 var day = document.getElementById("dayPlanner");
 var tmp = '';
-// localstorage object
-var schedule = {
-     name: "",
-     value:""
-};
 
 // set the date for the schedule
 setInterval(function(){ 
@@ -51,59 +46,46 @@ var hours =
 $.each( hours, function( index, value ){
      var hour = moment();
      var horita = hour.format('H');
-//console.log(typeof(horita));
      var currentTime = horita + 1;
-//console.log(currentTime + " :  " +index);
+
      tmp += '<div class="row time-block no-gutters mb-2">';
      tmp += '<div class="col-12 col-sm-2 hour">'+ value + '</div>';
      tmp += '<div class="col-12 col-sm-8">'; 
      if(index == currentTime){ //moment returns a string
-          tmp += '<textarea class="form-control present" id="textArea-'+ index + '" data-id="' + index + '" rows="3" placeholder="' + index + horita +'"></textarea>';
+          tmp += '<textarea class="form-control present" id="textArea-'+ index + '" data-id="' + index + '" rows="3"></textarea>';
      }else if(index > currentTime){
-          tmp += '<textarea class="form-control future" id="textArea-'+ index + '" data-id="' + index + '" rows="3" placeholder="' + index + horita +'"></textarea>';
+          tmp += '<textarea class="form-control future" id="textArea-'+ index + '" data-id="' + index + '" rows="3"></textarea>';
      } else if( index < currentTime) {
-          tmp += '<textarea class="form-control past" id="textArea-'+ index + '" data-id="' + index + '" rows="3" placeholder="' + index+ horita +'"></textarea>';
+          tmp += '<textarea class="form-control past" id="textArea-'+ index + '" data-id="' + index + '" rows="3"></textarea>';
      } else {
-          tmp += '<textarea class="form-control past" id="textArea-'+ index + '" data-id="' + index + '"  rows="3" placeholder="' + index+ horita +'"></textarea>';
+          tmp += '<textarea class="form-control past" id="textArea-'+ index + '" data-id="' + index + '"  rows="3"></textarea>';
      } 
      tmp += '</div><div class="col-12 col-sm-2 d-flex justify-content-around">';
      tmp += '<div style="" class="saveBtn w100 d-flex justify-content-center flex-fill">';
      tmp += '<div id="button" class="d-flex align-self-stretch align-items-center" data-save="'+ index +'"><i class="far fa-save"></i></div></div>';
      tmp += '</div></div>';
-},1000);
+});
 
 $(dayPlanner).prepend(tmp);
 
 $(document).on("click","#button", function(){
    
      $(".time-block #textArea-" + id).val();
-     //var id = $(this).data("id");
      var id = $(this).data("save");
-     //console.log($("#textArea-"+id).val());
-     //console.log($(this).val());
-     $(this).val();
-     //var value = $(this).val(),
-     //name = $(this).attr('data-id');
      var value = $("#textArea-"+id).val();
      var key = id;
-
-     //localStorage[name] = value;
-     //console.log('stored key: '+name+' stored value: '+value);
-     //localStorage.setItem("schedule", JSON.stringify(schedule));
-    //addToLocalStorage(name,'scheduele',value);
-    addToLocalStorage(key,value);
+     //add to local storage
+     addToLocalStorage(key,value);
 });
 
 
 var addToLocalStorage = function(key,value){
      //get existing data
      var existing = localStorage.getItem(schedule);
-
      // if there is data, create an array
      existing = existing ? JSON.parse(existing) : {};
      // add new data to localStorage Array
      existing[key] = value;
      //save back to localStorage
      localStorage.setItem(key, value);
-     //localStorage.setItem(name, JSON.stringify(existing));
 }
